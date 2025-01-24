807bf8b27d223fd2506780f12f90d236570c5fae
irfan wahendra <2111527002_irfan@student.unand.ac.id>
2025-01-24 09:29:20 +0700

    Initial Commit

---
+++ AlfaGiftTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlfaGiftTest
+//
+//  Created by irfan wahendra on 24/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
