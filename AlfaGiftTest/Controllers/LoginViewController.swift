51893adf30423f0738c5c31adb8e5e55df6142f8
irfan wahendra <2111527002_irfan@student.unand.ac.id>
2025-01-24 14:29:49 +0700

    some changes

--- AlfaGiftTest/Controllers/LoginViewController.swift
+++ AlfaGiftTest/Controllers/LoginViewController.swift
@@ -11,12 +11,28 @@ class LoginViewController: UIViewController {
 
     @IBOutlet weak var buttonLogin: UIButton!
     @IBOutlet weak var passwordField: UITextField!
-    @IBOutlet weak var usernameIfield: UITextField!
+    @IBOutlet weak var usernamefield: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    @IBAction func loginButtonTapped(_ sender: UIButton) {
+        guard let username = usernamefield.text, !username.isEmpty,
+                  let password = passwordField.text, !password.isEmpty else {
+                showAlert(message: "Username dan Password tidak boleh kosong")
+                return
+            }
 
+            // Validasi username & password
+            if username == "alfagift-admin" && password == "asdf" {
+                // Login berhasil
+                print("Login berhasil")
+            } else {
+                // Login gagal
+                showAlert(message: "Username atau password salah")
+            }
+        }
+    
 
 }
 
